df8c37637a43f3aad992189ccce45ad680de541e
Luna Cheney <puddalt@gmail.com>
2026-09-10 10:58:14 +1200

    fixing version number

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = "MC-Wiki"
 copyright = "2026, MongooseChat team"
 author = "MongooseChat team"
-release = "0.1"
+release = "0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
